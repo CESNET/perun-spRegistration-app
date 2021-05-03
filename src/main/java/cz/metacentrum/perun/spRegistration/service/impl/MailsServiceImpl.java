e16de1ef1bd92a1aec33e8641ba7ca161eb13409
Dominik Frantisek Bucik <bucik@ics.muni.cz>
2021-05-03 13:07:18 +0200

    TEST

--- src/main/java/cz/metacentrum/perun/spRegistration/service/impl/MailsServiceImpl.java
+++ src/main/java/cz/metacentrum/perun/spRegistration/service/impl/MailsServiceImpl.java
@@ -11,12 +11,25 @@ import cz.metacentrum.perun.spRegistration.service.mails.MailProperties;
 import cz.metacentrum.perun.spRegistration.service.mails.MailTemplate;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.internal.StringUtil;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+import org.jsoup.select.NodeTraversor;
+import org.jsoup.select.NodeVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
@@ -429,17 +442,109 @@ public class MailsServiceImpl implements MailsService {
 	private boolean sendMail(String to, String subject, String msg) {
 		try {
 			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8.toString());
-			helper.setFrom(mailProperties.getFrom());
-			helper.setTo(to);
-			helper.setSubject(subject);
-			helper.setText(msg, true);
+
+			Multipart mainMultipart = new MimeMultipart("mixed");
+			Multipart htmlAndTextMultipart = new MimeMultipart("alternative");
+
+			MimeBodyPart textBodyPart = new MimeBodyPart();
+			MimeBodyPart htmlBodyPart = new MimeBodyPart();
+			MimeBodyPart htmlAndTextBodyPart = new MimeBodyPart();
+
+			textBodyPart.setText(toPlainText(msg));
+			htmlAndTextMultipart.addBodyPart(textBodyPart);
+
+			htmlBodyPart.setContent(msg, "text/html; charset=utf-8");
+			htmlAndTextMultipart.addBodyPart(htmlBodyPart);
+
+			htmlAndTextBodyPart.setContent(htmlAndTextMultipart);
+			mainMultipart.addBodyPart(htmlAndTextBodyPart);
+
+			message.setContent(mainMultipart);
+			message.setFrom(mailProperties.getFrom());
+			message.setRecipients(Message.RecipientType.TO, to);
+			message.setSubject(subject);
 			mailSender.send(message);
 		} catch (Exception e) {
 			return false;
 		}
 
 		return true;
+	}
+
+	private String toPlainText(String htmlMsg) {
+		FormattingVisitor formatter = new FormattingVisitor();
+		NodeTraversor.traverse(formatter, Jsoup.parse(htmlMsg).body());
+
+		return formatter.toString();
+	}
+
+	// https://github.com/jhy/jsoup/blob/master/src/main/java/org/jsoup/examples/HtmlToPlainText.java
+	private static class FormattingVisitor implements NodeVisitor {
+		private static final int maxWidth = 80;
+		private int width = 0;
+		private final StringBuilder accum = new StringBuilder(); // holds the accumulated text
+
+		// hit when the node is first seen
+		public void head(Node node, int depth) {
+			String name = node.nodeName();
+			if (node instanceof TextNode) {
+				append(((TextNode) node).text()); // TextNodes carry all user-readable text in the DOM.
+			} else if (name.equals("li")) {
+				append("\n * ");
+			} else if (name.equals("dt")) {
+				append("  ");
+			} else if (StringUtil.in(name, "p", "h1", "h2", "h3", "h4", "h5", "tr",
+					"div", "ul", "ol", "br", "hr")) {
+				append("\n");
+			}
+		}
+
+		// hit when all of the node's children (if any) have been visited
+		public void tail(Node node, int depth) {
+			String name = node.nodeName();
+			if (StringUtil.in(name, "br", "dd", "dt", "p", "h1", "h2", "h3", "h4", "h5")) {
+				append("\n");
+			} else if (name.equals("a")) {
+				append(String.format(" <%s>", node.absUrl("href")));
+			}
+		}
+
+		// appends text to the string builder with a simple word wrap method
+		private void append(String text) {
+			if (text.startsWith("\n")) {
+				width = 0; // reset counter if starts with a newline. only from formats above, not in natural text
+			}
+			if (text.equals(" ") &&
+					(accum.length() == 0 || StringUtil.in(accum.substring(accum.length() - 1), " ", "\n"))) {
+				return; // don't accumulate long runs of empty spaces
+			}
+
+			if (text.length() + width > maxWidth) { // won't fit, needs to wrap
+				String[] words = text.split("\\s+");
+				for (int i = 0; i < words.length; i++) {
+					String word = words[i];
+					boolean last = i == words.length - 1;
+					if (!last) { // insert a space if not the last word
+						word = word + " ";
+					}
+					if (word.length() + width > maxWidth) { // wrap and reset counter
+						accum.append("\n").append(word);
+						width = word.length();
+					} else {
+						accum.append(word);
+						width += word.length();
+					}
+				}
+			} else { // fits as is, without need to wrap text
+				accum.append(text);
+				width += text.length();
+			}
+		}
+
+		@Override
+		public String toString() {
+			return accum.toString();
+		}
 	}
 
 }
