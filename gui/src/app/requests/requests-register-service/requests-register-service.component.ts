0e3e46e4d346d4a630ff5631113e76e25d490818
Dominik František Bučík <bucik@ics.muni.cz>
2022-07-04 11:43:41 +0200

    fix: 🐛 displaying new service form (preselection of protocol)

--- gui/src/app/requests/requests-register-service/requests-register-service.component.ts
+++ gui/src/app/requests/requests-register-service/requests-register-service.component.ts
@@ -1,5 +1,5 @@
 import {
-  Component,
+  Component, OnDestroy,
   OnInit,
   QueryList,
   ViewChild,
@@ -14,13 +14,14 @@ import { TranslateService } from '@ngx-translate/core'
 import { PerunAttribute } from '../../core/models/PerunAttribute'
 import { RequestsRegisterServiceStepComponent } from './requests-register-service-step/requests-register-service-step.component'
 import { Router } from '@angular/router'
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-new-request',
   templateUrl: './requests-register-service.component.html',
   styleUrls: ['./requests-register-service.component.scss']
 })
-export class RequestsRegisterServiceComponent implements OnInit {
+export class RequestsRegisterServiceComponent implements OnInit, OnDestroy {
   constructor (
     private configService: ConfigService,
     private requestsService: RequestsService,
@@ -29,25 +30,25 @@ export class RequestsRegisterServiceComponent implements OnInit {
     private router: Router
   ) {}
 
-  @ViewChildren(RequestsRegisterServiceStepComponent)
-    steps: QueryList<RequestsRegisterServiceStepComponent>
+  @ViewChildren(RequestsRegisterServiceStepComponent) steps: QueryList<RequestsRegisterServiceStepComponent>;
 
-  @ViewChild(MatStepper, { static: false })
-    stepper: MatStepper
+  @ViewChild(MatStepper, { static: false }) stepper: MatStepper
 
-  serviceSelected: string
+  enabledProtocolsSubscription: Subscription;
+  errorMessageSubscription: Subscription;
+  successMessageSubscription: Subscription;
 
-  isFormVisible = false
-  isCardBodyVisible = false
-  enabledProtocols: string[]
-  loading = true
-  selected = ''
+  serviceSelected: string;
+
+  formVisible = false;
+  enabledProtocols: string[];
+  loading = true;
 
   // translations
-  errorText: string
-  successActionText: string
+  errorText: string;
+  successActionText: string;
 
-  applicationItemGroups: ApplicationItem[][]
+  applicationItemGroups: ApplicationItem[][];
 
   /**
    * Filters items that should not be displayed
@@ -55,21 +56,21 @@ export class RequestsRegisterServiceComponent implements OnInit {
    * @param items
    */
   private static filterItems (items: ApplicationItem[][]): ApplicationItem[][] {
-    const filteredItems: ApplicationItem[][] = []
+    const filteredItems: ApplicationItem[][] = [];
 
     items.forEach((itemsGroup) => {
-      const filteredGroup: ApplicationItem[] = []
+      const filteredGroup: ApplicationItem[] = [];
 
       itemsGroup.forEach((item) => {
         if (item.displayed) {
-          filteredGroup.push(item)
+          filteredGroup.push(item);
         }
-      })
+      });
 
-      filteredItems.push(filteredGroup)
+      filteredItems.push(filteredGroup);
     })
 
-    return filteredItems
+    return filteredItems;
   }
 
   /**
@@ -78,57 +79,76 @@ export class RequestsRegisterServiceComponent implements OnInit {
    * @param items
    */
   private static sortItems (items: ApplicationItem[][]): ApplicationItem[][] {
-    const sortedItems: ApplicationItem[][] = []
+    const sortedItems: ApplicationItem[][] = [];
 
     items.forEach((itemsGroup) => {
       sortedItems.push(
         itemsGroup.sort((a, b) => a.displayPosition - b.displayPosition)
-      )
-    })
+      );
+    });
 
-    return sortedItems
+    return sortedItems;
   }
 
   ngOnInit () {
-    this.configService.getProtocolsEnabled().subscribe(
-      (protocols) => {
-        this.enabledProtocols = protocols
-        this.loading = false
-        this.enabledProtocols.forEach((v) => v.toLowerCase())
+    this.enabledProtocolsSubscription = this.configService.getProtocolsEnabled().subscribe((protocols) => {
+        this.enabledProtocols = [];
+        if (protocols) {
+          protocols.forEach(v => {
+            if (v) {
+              this.enabledProtocols.push(v.toLowerCase());
+            }
+          });
+        }
+        if (!this.enabledProtocols || this.enabledProtocols.length === 0) {
+          console.log("Error, no protocols enabled");
+        }
         if (this.enabledProtocols.indexOf('oidc') === -1) {
           this.samlSelected()
         } else if (this.enabledProtocols.indexOf('saml') === -1) {
           this.oidcSelected()
         }
+        this.loading = false;
       },
       (error) => {
-        this.loading = false
-        console.log(error)
+        this.loading = false;
+        console.log(error);
       }
-    )
+    );
 
-    this.translate
+    this.errorMessageSubscription = this.translate
       .get('REQUESTS.ERRORS.VALUES_ERROR_MESSAGE')
-      .subscribe((value) => (this.errorText = value))
-    this.translate
+      .subscribe((value) => (this.errorText = value));
+    this.successMessageSubscription = this.translate
       .get('REQUESTS.SUCCESSFULLY_SUBMITTED')
-      .subscribe((value) => (this.successActionText = value))
+      .subscribe((value) => (this.successActionText = value));
+  }
+
+  ngOnDestroy(): void {
+    if (this.enabledProtocolsSubscription) {
+      this.enabledProtocolsSubscription.unsubscribe();
+    }
+    if (this.errorMessageSubscription) {
+      this.errorMessageSubscription.unsubscribe();
+    }
+    if (this.successMessageSubscription) {
+      this.successMessageSubscription.unsubscribe();
+    }
   }
 
   revealForm () {
-    this.loading = false
-    this.isCardBodyVisible = true
-    this.isFormVisible = true
+    this.loading = false;
+    this.formVisible = true;
   }
 
   onLoading () {
     this.loading = true
-    this.isCardBodyVisible = false
+    this.formVisible = false
   }
 
   oidcSelected () {
-    this.onLoading()
-    this.selected = 'oidc'
+    this.onLoading();
+    this.serviceSelected = 'oidc';
 
     this.configService.getOidcApplicationItems().subscribe((items) => {
       items = items.map((category) =>
@@ -136,52 +156,51 @@ export class RequestsRegisterServiceComponent implements OnInit {
       )
       this.applicationItemGroups = RequestsRegisterServiceComponent.sortItems(
         RequestsRegisterServiceComponent.filterItems(items)
-      )
-      this.revealForm()
+      );
+      this.revealForm();
     })
   }
 
   samlSelected () {
-    this.onLoading()
-    this.selected = 'saml'
+    this.onLoading();
+    this.serviceSelected = 'saml';
 
     this.configService.getSamlApplicationItems().subscribe((items) => {
       items = items.map((category) =>
         category.map((item) => new ApplicationItem(item))
-      )
+      );
       this.applicationItemGroups = RequestsRegisterServiceComponent.sortItems(
         RequestsRegisterServiceComponent.filterItems(items)
       )
-      this.revealForm()
-    })
+      this.revealForm();
+    });
   }
 
   /**
    * Collects data from form and submits new request
    */
   submitRequest () {
-    this.loading = true
-    let perunAttributes: PerunAttribute[] = []
+    this.loading = true;
+    let perunAttributes: PerunAttribute[] = [];
 
     this.steps.forEach(
-      (step) =>
-        (perunAttributes = perunAttributes.concat(step.getPerunAttributes()))
-    )
+      (step) => (perunAttributes = perunAttributes.concat(step.getPerunAttributes()))
+    );
 
     this.requestsService.createRegistrationRequest(perunAttributes).subscribe(
       (requestId) => {
         this.loading = false
-        this.snackBar.open(this.successActionText, null, { duration: 6000 })
-        this.router.navigate(['/auth/requests/detail/' + requestId])
+        this.snackBar.open(this.successActionText, null, { duration: 6000 });
+        this.router.navigate(['/auth/requests/detail/' + requestId]);
       },
       (error) => {
-        this.loading = false
-        console.log(error)
+        this.loading = false;
+        console.log(error);
       }
     )
   }
 
   previousStep () {
-    this.stepper.previous()
+    this.stepper.previous();
   }
 }
